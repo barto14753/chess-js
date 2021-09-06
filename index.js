583b4394cdebc279f7004be0ef604119ddb716d5
barto14753 <wlodarski_bartosz@o2.pl>
2021-09-07 00:24:21 +0200

    handling sidebar buttons on client side

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
@@ -8,12 +7,84 @@ const io = new Server(server);
 
 users = Array();
 
+const USER_IMAGES_PATH = 'img/faces/';
+const USER_IMAGES = [
+  'face1.png',
+  'face2.png',
+  'face3.png',
+  'face4.png',
+  'face5.png',
+  'face6.png',
+]
+
+function getIndex(id)
+{
+  for (let i=0; i<users.length; i++)
+  {
+    if (users[i].id == id)
+    {
+      return i;
+    }
+  }
+  return -1;
+}
+
+
+function deleteUser(id)
+{
+  let index = getIndex(id);
+
+    if (index > -1) {
+      users.splice(index, 1);
+    }
+    console.log(users);
+}
+
+
+
+
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
+
+
+io.on('connection', (socket) => {
+  let user_obj = {
+    "id": null,
+    "username": null,
+    "joined": new Date().toLocaleTimeString(),
+    "photo": USER_IMAGES_PATH + USER_IMAGES[Math.floor(Math.random()*USER_IMAGES.length)]
+  }
+
+  console.log('New user connected'); 
+  socket.emit('init', users, user_obj);
+  console.log(users);
+
+
+  socket.on('disconnect', () => {
+    deleteUser(user_obj.id);
+    socket.broadcast.emit('deleteUser', user_obj)
+    console.log("User %s disconnected", user_obj.username);
+
+  });
+
+  socket.on('setDetails', (u, i) => {
+    user_obj.username = u;
+    user_obj.id = i;
+
+    users.push(user_obj);
+    console.log("User set username: %s", user_obj.id, user_obj.username);
+    console.log(users);
+    socket.broadcast.emit('newUser', user_obj);
+
+  });
+
+});
+
+
 
 server.listen(3000, () => {
     console.log('Listening on port 3000');
--- public/script.js
+++ public/script.js
@@ -1,3 +1,118 @@
+
+let username = prompt("Username: ");
+if (username.length == 0) username = "Anonymous"
+
+let socket = io();
+let user;
+let users = Array();
+
+
+$('.sidebar-button').click((event) => {
+  let id = $(event.target).parent().parent().attr("id");
+  let mode = ($(event.target).hasClass('badge-secondary')) ? "SEND" : "CANCEL";
+
+  let info = 
+  {
+    "sender_id": user.id,
+    "receiver_id": id
+  }
+
+  if (mode == "SEND")
+  {
+    socket.emit("SEND REQUEST", info);
+    $(event.target).removeClass('badge-secondary');
+    $(event.target).addClass('badge-danger');
+  }
+  else // CANCEL
+  {
+    socket.emit("CANCEL REQUEST", info);
+    $(event.target).removeClass('badge-danger');
+    $(event.target).addClass('badge-secondary');
+  }
+})
+
+function getUser(user_id)
+{
+    for (const user of users)
+    {
+        if (user.id === user_id) return user;
+    }
+
+    return null;
+}
+
+function onCreateYourself()
+{
+  console.log("On create yourself", user);
+  $('.user-photo').attr('src', user.photo);
+  $('.username-text').text(user.username);
+}
+
+function onNewUser(user)
+{
+  console.log("On new user", user);
+  let newUser = $('#sidebar-user-template').clone(true);
+  newUser.attr("id", user.id);
+  newUser.removeClass("d-none");
+  newUser.find('.user-image').attr("src", user.photo);
+  newUser.find('.sidebar-username').text(user.username);
+
+  $('#sidebar-users-list').append(newUser);
+}
+
+function onDeleteUser(user_id)
+{
+  console.log("On delete user", user_id);
+
+  // besides sidebar it will remove all elements with user_id (values in other columns should have user_id)
+  $('#' + user_id).remove();
+}
+
+
+
+
+// COMMUNICATION WITH SERVER
+
+socket.on('init', function(u, user_obj) {
+  user = {
+      "id": socket.id,
+      "username": username,
+      "joined": user_obj.joined,
+      "photo": user_obj.photo
+  }
+  socket.emit("setDetails", username, socket.id, );
+  onCreateYourself();
+
+  for (const user of u) onNewUser(user);
+
+});
+
+socket.on('newUser', function(user) {
+  onNewUser(user);
+});
+
+socket.on('deleteUser', function(user) {
+  onDeleteUser(user.id);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 jQuery(function ($) {
 
     $(".sidebar-dropdown > a").click(function() {
@@ -114,3 +229,6 @@ var config = {
 board = Chessboard('myBoard', config)
 
 updateStatus()
+
+
+
